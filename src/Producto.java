0063b480581e032bdfeea1999d23e36469b68828
YoselynBascunan <Yoselyn.bascunan1201@gmail.com>
2024-10-16 10:54:13 -0300

    Initial commit

---
+++ src/Producto.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public abstract class Producto {
+    protected double precio;
+    protected int cantidadDisponible;
+
+    public Producto(double precio, int cantidadDisponible) {
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public abstract void mostrarDetalles();
+}
